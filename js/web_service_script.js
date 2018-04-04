4487218c85c47f772aa2536e0a4570d7ad97f2be
BuiNgoc <ngoc.buiquang@gmail.com.vn>
2018-04-04 16:29:28 +0700

    init

--- js/web_service_script.js
+++ js/web_service_script.js
@@ -3,6 +3,6 @@ console.info("Detect WebService");
 var elem = document.createElement("div");
 elem.id = '_version-add-on';
 elem.setAttribute("data-last-updated-time", last_updated_time);
-elem.setAttribute("style", "display: none;");
+elem.setAttribute("style", "display: block;");
 elem.innerHTML = version_tool;
 document.body.insertBefore(elem, document.body.childNodes[0]);
